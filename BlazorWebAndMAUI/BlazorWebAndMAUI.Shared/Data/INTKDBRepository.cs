0d7c25a49659a1f87eed9bb4a35db2904f0347d6
RaspeR87 <rasper87@outlook.com>
2022-10-06 13:35:36 +0200

    Shared project commit

---
+++ BlazorWebAndMAUI/BlazorWebAndMAUI.Shared/Data/INTKDBRepository.cs
@@ -0,0 +1,16 @@
+﻿using BlazorWebAndMAUI.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWebAndMAUI.Shared.Data
+{
+    public interface INTKDBRepository
+    {
+        public Task AddMnenje(Mnenje mnenje);
+
+        public Task<List<Mnenje>> GetAllMnenje();
+    }
+}
---
+++ BlazorWebAndMAUI/BlazorWebAndMAUI.Shared/Models/Mnenje.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWebAndMAUI.Shared.Models
+{
+    public class Mnenje
+    {
+        public Guid ID { get; set; }
+
+        [Required(ErrorMessage = "Polje 'ime' je obvezno."), MinLength(3, ErrorMessage = "Minimalna dolžina imena je 3 znake."), MaxLength(100, ErrorMessage = "Maksimalna dolžina imena je 100 znakov.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Polje 'Moj TOP Blazor' je obvezno.")]
+        public string? MojBlazor { get; set; }
+    }
+}
